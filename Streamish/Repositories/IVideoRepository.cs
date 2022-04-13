ad46b34761ae93b7b3c3e272776900f18dcb4b68
Sharif Rashed <sharif.rashed26@gmail.com>
2022-04-13 10:03:13 -0500

    initial commit

---
+++ Streamish/Repositories/IVideoRepository.cs
@@ -0,0 +1,16 @@
+﻿using Streamish.Controllers;
+using Streamish.Models;
+using System.Collections.Generic;
+
+namespace Streamish.Repositories
+{
+    public interface IVideoRepository
+    {
+        List<Video> GetAll();
+        Video GetById(int id);
+
+        public void Update(Video video);
+        public void Delete(int id);
+        public void Add(Video video);
+    }
+}
